06c3d896f052874c41080576d4da59a5e97857f8
Alejandro Astu <jastudiagu@educacion.navarra.es>
2023-12-14 16:07:44 +0100

    Ejercicio 4 (14/12)

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -3,17 +3,12 @@ import urllib.request
 
 def lee_url(url):
     archivo = urllib.request.urlopen(url)
-    cont = 0
     for line in archivo:
         decoded_line = line.decode("utf-8")
-        cont += 1
         print(decoded_line)
-    
-    print(cont)
     return
 
 
 url = "http://textfiles.com/adventure/aencounter.txt"
 
 lee_url(url)
-
